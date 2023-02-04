0137d2ccac26f15f7b49c5e1320153277312558a
GuiProgrammer <meucomput.2365@gmail.com>
2023-02-04 20:51:03 -0300

    first commit

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual é a primeira lei de Newton ?",
+      answers: [
+        { option: "Lei da Força", correct: false },
+        { option: "Lei da Inércia", correct: true },
+        { option: "Lei da Ação e Reação", correct: false },
+      ],
+    },
+    {
+      question: "Qual a fórmula da Carga Elétrica?",
+      answers: [
+        { option: "Q = m.a", correct: false },
+        { option: "Q = n.e", correct: true },
+        { option: "Q = f.d", correct: false },
+      ],
+    },
+    {
+      question: "Qual desses não é um físico? ",
+      answers: [
+        { option: "Nikola Tesla", correct: false },
+        { option: "Albert Einstein", correct: false },
+        { option: "Goleiro Bruno", correct: true },
+      ],
+    },
+    {
+      question: "Quem criou a teoria da relatividade",
+      answers: [
+        { option: "Stephen Hawking", correct: false },
+        { option: "Mário Schenberg", correct: false },
+        { option: "Albert Einstein", correct: true },
+      ],
+    },
+  ];
